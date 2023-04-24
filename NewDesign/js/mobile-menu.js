29d298ec4e0b6a0f9e6a73820305b3a3e3e49925
Ruslan <saharcorez@gmail.com>
2023-04-24 22:21:35 +0300

    Анимация крестика

--- NewDesign/js/mobile-menu.js
+++ NewDesign/js/mobile-menu.js
@@ -5,6 +5,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const swipeMenuContainer = swipeMenu.querySelector(".swipe-menu__container");
   const menuBackground = document.querySelector(".menu-background");
   const mobileMenu = document.querySelector(".mobile-menu");
+  const burgerIcon = document.querySelector(".menu__button-burger-ico");
 
   function handleMobileMenu() {
     mobileMenuButtonService.classList.toggle("mobile-menu__button-service_active");
@@ -26,6 +27,7 @@ window.addEventListener("DOMContentLoaded", function () {
     swipeMenu.classList.remove("swipe-menu_active");
     menuBackground.classList.remove("menu-background_active");
     swipeMenu.style.transform = `translateY(150%)`;
+    burgerIcon.classList.remove("burger_active");
   }
 
   let touchstartY = 0;
@@ -33,36 +35,40 @@ window.addEventListener("DOMContentLoaded", function () {
   let movedY = 0;
   let containerPos = 0;
 
-  swipeMenu.addEventListener('touchstart', function (event) {
-    touchstartY = event.changedTouches[0].screenY;
-  }, false)
+  if (swipeMenu) {
+    swipeMenu.addEventListener('touchstart', function (event) {
+      touchstartY = event.changedTouches[0].screenY;
+    }, false)
 
-  swipeMenu.addEventListener('touchmove', function (event) {
-    movedY = event.changedTouches[0].screenY;
-    if ((movedY - touchstartY > 0) && (!event.target.closest(".swipe-menu__container"))) {
-      swipeMenu.style.transform = `translateY(${movedY - touchstartY}px)`;
-    }
-  })
+    swipeMenu.addEventListener('touchmove', function (event) {
+      movedY = event.changedTouches[0].screenY;
+      if ((movedY - touchstartY > 0) && (!event.target.closest(".swipe-menu__container"))) {
+        swipeMenu.style.transform = `translateY(${movedY - touchstartY}px)`;
+      }
+    })
 
-  swipeMenuContainer.addEventListener("scroll", () => {
-    containerPos = swipeMenuContainer.scrollTop;
-  })
+    swipeMenuContainer.addEventListener("scroll", () => {
+      containerPos = swipeMenuContainer.scrollTop;
+    })
 
-  swipeMenu.addEventListener('touchend', function (event) {
-    touchendY = event.changedTouches[0].screenY;
+    swipeMenu.addEventListener('touchend', function (event) {
+      touchendY = event.changedTouches[0].screenY;
 
-    if ((movedY - touchstartY > 30) && (!event.target.closest(".swipe-menu__container"))) {
-      mobileMenuClose();
-    } else {
-      swipeMenu.style.transform = `translateY(0)`;
-    }
-  }, false)
+      if ((movedY - touchstartY > 30) && (!event.target.closest(".swipe-menu__container"))) {
+        mobileMenuClose();
+      } else {
+        swipeMenu.style.transform = `translateY(0)`;
+      }
+    }, false)
 
-  mobileMenuButtonService.addEventListener("click", handleMobileMenu);
+    mobileMenuButtonService.addEventListener("click", handleMobileMenu);
+  }
 
   window.addEventListener("scroll", () => {
-    if (!mobileMenu.classList.contains("mobile-menu_opened")) {
-      mobileMenu.classList.add("mobile-menu_opened");
+    if (mobileMenu) {
+      if (!mobileMenu.classList.contains("mobile-menu_opened")) {
+        mobileMenu.classList.add("mobile-menu_opened");
+      }
     }
   })
 
@@ -73,6 +79,7 @@ window.addEventListener("DOMContentLoaded", function () {
       swipeMenu.classList.remove("swipe-menu_active");
       menuBackground.classList.remove("menu-background_active");
       swipeMenu.style.transform = `translateY(150%)`;
+      burgerIcon.classList.remove("burger_active");
     }
 
     if (html.style.overflow === "hidden") {
